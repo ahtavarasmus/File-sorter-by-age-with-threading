d9b5904864f83c805bb5f706893cb885ec5bbcbc
ahtavarasmus <76125307+ahtavarasmus@users.noreply.github.com>
2021-07-11 12:38:55 +0300

    Add files via upload

---
+++ sortingFilesByAge.py
@@ -0,0 +1,59 @@
+import os
+import time
+import datetime
+import threading
+from pathlib import Path
+import shutil
+
+
+def movingTheFile(start, end):
+
+    for posNum in range(start, end):
+        og_path = filenamelist[posNum]
+        # getting the last modified time as a datetime:
+        # mtime = 1585501464.0
+        mtime = os.path.getmtime(og_path)
+        date = time.ctime(mtime)  # date = Sun Mar 29 20:04:24 2020'
+
+        # getting the datetime object
+        # datetime.datetime(2020, 3, 29, 20, 4, 24)
+        datetimeObj = datetime.datetime.strptime(date, '%a %b %d %H:%M:%S %Y')
+
+        # Getting a year from datetimeObj
+        year = datetimeObj.year  # year = 2020
+        month = datetimeObj.strftime('%B')  # month = march
+
+        filename = os.path.basename(og_path)
+        newPath = 'D:\\sortedtest1\\{}\\{}'.format(year, month)
+
+        Path(newPath).mkdir(parents=True, exist_ok=True)  # windowsPath object
+
+        newfilePath = os.path.join(newPath, filename)
+        print('Copying {} to {}'.format(og_path, newfilePath))
+        shutil.copy(og_path, newfilePath)
+
+
+filenamelist = []  # list of all the filenames in a drive
+for foldername, subfolders, filenames in os.walk('D:\\videot\\random'):
+    for filename in filenames:
+        full_path = os.path.join(foldername, filename)
+        filenamelist.append(full_path)
+# Create and start the Thread objects.
+downloadThreads = []
+listLen = len(filenamelist)
+for i in range(0, listLen + 1, 10):
+    start = i
+    if (listLen - i) < 10:  # the last ten files won't use threads
+        end = i + (listLen-i)
+        movingTheFile(start, end)
+    else:
+        end = i + 9
+        downloadThread = threading.Thread(
+            target=movingTheFile, args=(start, end))
+        downloadThreads.append(downloadThread)
+        downloadThread.start()
+
+# Wait for all threads to end.
+for downloadThread in downloadThreads:
+    downloadThread.join()
+print('Done.')
